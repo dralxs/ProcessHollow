e05f79814ec5bf39dccdbee921f87de44fbefbbe
dralxs <dr.ales@protonmail.com>
2023-10-01 19:46:45 +0200

    [feat] delegates win32/winhttp

---
+++ DLL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using DInvoke.DynamicInvoke;
+
+namespace ProcessHollow
+{
+    internal class DLL
+    {
+        public string name;
+
+        public object ChaseFunction(string fname)
+        {
+            var type = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                        from t in assembly.GetTypes()
+                        where t.Name == fname
+                        select t).FirstOrDefault();
+            this.CheckNull(type, fname + " not found");
+            var p = Generic.GetLibraryAddress(this.name, fname, true);
+            this.CheckNullPtr(p, fname);
+            var x = Marshal.GetDelegateForFunctionPointer(p, type);
+            this.CheckNull(x, "GetDelegateForFunctionPointer");
+            return x;
+        }
+
+        public DLL(string name)
+        {
+            this.name = name;
+        }
+
+        public void CheckNull(object test, string label)
+        {
+            if (test == null)
+            {
+                Console.WriteLine("Error: {0} is null", label);
+                Environment.Exit(1);
+            }
+        }
+
+        public void CheckNullPtr(IntPtr test, string label)
+        {
+            if (test == IntPtr.Zero)
+            {
+                Console.WriteLine("Error: {0} is INtPtr.Zero", label);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using DInvoke.DynamicInvoke;
+
+namespace ProcessHollow
+{
+    internal class Program
+    { 
+        public const uint WINHTTP_ACCESS_TYPE_DEFAULT_PROXY = 0;
+        public const uint WINHTTP_FLAG_ASYNC = 0x10000000;
+        public static void Main(string[] args)
+        {
+            DLL wh = new DLL("winhttp.dll");
+            var openConnWinHttp = wh.ChaseFunction("WinHttpOpen") as WinHttpOpen;
+            var hConn = openConnWinHttp("useragent", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, null, null, WINHTTP_FLAG_ASYNC);
+
+            if (hConn == IntPtr.Zero)
+            {
+                Console.WriteLine("Failed to open session. Error: " + Marshal.GetLastWin32Error());
+            }
+            else
+            {
+                Console.WriteLine("Session opened successfully!");
+            }
+
+        }
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UInt32 lpNumberOfBytesWritten);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate IntPtr WinHttpOpen(string pszAgentW, uint dwAccessType, string pszProxyW, string pszProxyBypassW, uint dwFlags);
+
+}
